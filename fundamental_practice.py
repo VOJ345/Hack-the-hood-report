b8ee39a4dbd4c3abc6ac9f1b0f116bc1773133a3
VOJ345 <103322110+VOJ345@users.noreply.github.com>
2022-04-10 18:24:32 -0700

    Create fundamental_practice.py

---
+++ fundamental_practice.py
@@ -0,0 +1,7 @@
+print()# this is a fundamental practice to understand codes
+
+print("Hello world")
+
+"car_name" 
+"ford mustang" #price_US$27.000_Potencia_310_to_470_CV 
+"MOTOR 2.3L 4 Motor_in_Line_5.0 L v8"
