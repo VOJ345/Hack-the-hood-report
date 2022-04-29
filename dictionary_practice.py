d3835101a754b92a363d431ed4f663749c99dedd
VOJ345 <103322110+VOJ345@users.noreply.github.com>
2022-04-28 22:07:51 -0700

    dictionary practice

--- dictionary_practice.py
+++ dictionary_practice.py
@@ -1,29 +1,60 @@
-Food_prices = {"Chicken" : "$1.59", 
-               "Beef" : "$1.99", 
-               "Cheese" : "$1.00", 
-               "Milk" : "$2.50"}
-               
+from tkinter import X
 
-Consoles = {"Ps5" : "$1000",
-            "Ps4" : "$300",
-            "Xbox one" : "$350",
-            "Xbox X" : "$600"}   
+
+Food_prices = {
+    "Chicken" : "1.59", 
+    "Beef" : "1.99", 
+    "Cheese" : "1.00", 
+    "Milk" : "2.50"
+}
  
+               
+Consoles = {
+    "Ps5" : "1000",
+    "Ps4" : "300",
+    "Xbox one" : "350",
+    "Xbox X" : "600"
+}
 
-Chicken = Food_prices["Chicken"]
+Ps5_prices = Consoles["Ps5"]
+Ps4_prices = Consoles["Ps4"]
+Xbox_one_prices = Consoles["Xbox one"]
+print(Consoles ["Ps5"])
 
-Beef = Food_prices["Beef"]
+shoe_inventory = {
+    "Jordan 13": 1, 
+    "Yeezy": 8,
+    "Foamposite": 10,
+    "Air Max": 5,
+    "SB Dunk": 20,
 
-Cheese = Food_prices["Cheese"]
+}
+#costumer purchase 2 pairs of SB Dunks and Came to return a pair of Yeezy
+shoe_inventory["SB Dunk"] -= 2
+shoe_inventory["Yeezy"] += 1
+print(shoe_inventory)
 
-Milk = Food_prices["Milk"]
-print(Chicken, Beef, Cheese, Milk)
+#shoes inventory Increases by 7
+shoe_inventory["Jordan 13"] *= 7
+shoe_inventory["Yeezy"] *= 7
+shoe_inventory["Foamposite"] *=7
+shoe_inventory["Air Max"] *= 7
+shoe_inventory["SB Dunk"] *= 7
+print(shoe_inventory)
 
-Ps5= Consoles["Ps5"]
+#shoes inventory decreases decreases by 3
+shoe_inventory["Jordan 13"] -= 3
+shoe_inventory["Yeezy"] -= 3
+shoe_inventory["Foamposite"] -= 3
+shoe_inventory["Air Max"] -= 3
+shoe_inventory["SB Dunk"] -= 3
+print(shoe_inventory)
 
-Ps4 = Consoles["Ps4"]
+#adding new shoes 
 
-Xbox_one = Consoles["Xbox one"]
-print(Ps5,Ps4,Xbox_one)
+shoe_inventory["Nike"] = 3
+shoe_inventory["Vans"] = 2
+shoe_inventory["Aldo"] = 1
+print(shoe_inventory) 
 
 
