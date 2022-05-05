c900ca2a54c39fb0b99dff31b9040bbb28b3ba29
VOJ345 <103322110+VOJ345@users.noreply.github.com>
2022-05-04 17:13:05 -0700

    fundamental_practice

---
+++ fundamental_practice.py
@@ -0,0 +1,18 @@
+print("hello world")# this is a fundamental practice to understand codes
+
+name = "Henry" # string 
+age = 25 # integer 
+years_at_hth = 2.5 # float 
+plays_basketball = True # boolean
+jersey_number ="13" # string 
+
+car_name = "Ford" # String
+x = 50 # integer 
+
+print(f"{name} is a {type(name)}") 
+print(f"{age} is a {type(age)}") 
+print(f"{years_at_hth} is a {type(years_at_hth)}") 
+print(f"{plays_basketball} is a {type(plays_basketball)}") 
+print(f"{jersey_number} is a {type(jersey_number)}") 
+print(f"{car_name} is a {type(name)}") 
+print(f"{x} is a {type(x)}") 
